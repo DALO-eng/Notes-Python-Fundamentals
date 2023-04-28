b02049fae3670b3a6c25fff6e9a36890eff5b883
Daniel Leon <dalobusiness01@gmail.com>
2023-04-28 15:55:10 -0500

    [ADD] Added first lab

--- print.py
+++ print.py
@@ -1,2 +1,16 @@
 # Print allows you to print a string to the console
 print("Hello World"); # Prints Hello World to the console
+
+# Escape Characters
+print("This is a backslash: \\"); # This is a backslash: \
+print("This is a double quote: \""); # This is a double quote: "
+print("This is a single quote: \'"); # This is a single quote: '
+print("This is a tab: \t"); # This is a tab:
+print("This is a new line: \n"); # This is a new line:
+
+# Multiple arguments to print()
+print("Hello", "World"); # Hello World
+
+# Keyword arguments to print()
+print("Hello", "World", sep="***"); # Hello***World
+print("Hello", "World", end="***"); # Hello World***
