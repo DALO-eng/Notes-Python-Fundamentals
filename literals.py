b469109c89e47248b6105535aa353eb15c0939e8
Daniel Leon <dalobusiness01@gmail.com>
2023-04-29 20:26:15 -0500

    [ADD] Added floats literals

--- literals.py
+++ literals.py
@@ -11,3 +11,12 @@ print(1_00); # Underscore is ignored
 print(0b110101); # Binary to Decimal
 print(0o123); # Octal to Decimal
 print(0xAB); # Hexadecimal to Decimal
+
+# Float Literals
+print(0.0); # 0 is not equal to 0.0
+print(-0.0);
+print(25.5);
+print(10.); # Decimal point is required
+print(1_00.5); # Underscore is ignored
+print(1e10); # Exponent form of 1 * 10^10
+print(6.62607E-34); # Exponent form of 6.62607 * 10^-34
