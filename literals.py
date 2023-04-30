36fd55be4275cf94f5a4cc8ca1f6343bcd1244fc
Daniel Leon <dalobusiness01@gmail.com>
2023-04-29 20:20:01 -0500

    [ADD] Added literals

---
+++ literals.py
@@ -0,0 +1,13 @@
+# Python Literals
+# Literals can be defined as a data that is given in a variable or constant.
+
+# Python supports the following literals:
+
+# Integer Literals
+print(25);
+print(-25);
+print(100);
+print(1_00); # Underscore is ignored
+print(0b110101); # Binary to Decimal
+print(0o123); # Octal to Decimal
+print(0xAB); # Hexadecimal to Decimal
