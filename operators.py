8a793bdaf1fa03bf5edd5593cb5ebf09834eeb6f
Daniel Leon <dalobusiness01@gmail.com>
2023-04-30 20:15:58 -0500

    [ADD] Added operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+# Operators
+
+# Arithmetic Operators
+# +, -, *, /, %, **, //
+print(10 + 3);
+print(10 - 3);
+print(10 * 3);
+print(10 / 3);
+print(10 % 3);
+print(10 ** 3); # ! Exponent right to left
+print(10 // 3); # Floor division
+
+# Priority
+# 1. Parenthesis
+# 2. Exponentiation
+# 3. Multiplication or Division
+# 4. Addition or Subtraction
